f7dc2af1377379dc5d6b14b1c834f7ee152f2952
Nataly Mironova <knightalli@mail.ru>
2024-11-11 01:42:48 +0500

    add DeckOpenButton

---
+++ Assets/Game/UI/HandButtons/DeckOpenButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckOpenButton : CustomButton
+{
+    protected override void OnClick()
+    {
+        //здесть бует открытие колоды
+        throw new System.NotImplementedException();
+    }
+}
