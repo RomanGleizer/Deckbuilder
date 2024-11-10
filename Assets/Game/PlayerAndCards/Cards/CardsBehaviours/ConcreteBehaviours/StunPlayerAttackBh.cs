69c9399f62544d3c595283579553f47a221b53bf
Roman <125725530+RomanGleizer@users.noreply.github.com>
2024-11-10 19:08:48 +0500

    Update Field.cs

--- Assets/Game/PlayerAndCards/Cards/CardsBehaviours/ConcreteBehaviours/StunPlayerAttackBh.cs
+++ Assets/Game/PlayerAndCards/Cards/CardsBehaviours/ConcreteBehaviours/StunPlayerAttackBh.cs
@@ -1,16 +1,9 @@
-﻿using Game.PlayerAndCards.PlayerScripts;
-using Game.PlayerAndCards.PlayerScripts.Interfaces;
-
-namespace Game.PlayerAndCards.Cards.CardsBehaviours.ConcreteBehaviours
+﻿public class StunPlayerAttackBh : ISpecialAttackBh
 {
-    public class StunPlayerAttackBh : ISpecialAttackBh
-    {
-        private Player _player;
-        private IStunnable _stunnablePlayer;
+    // private IStunnable _stunnablePlayer;
 
-        public void Attack()
-        {
-            _player.Stun(1f);
-        }
+    public void Attack()
+    {
+        // _player.Stun();
     }
 }
