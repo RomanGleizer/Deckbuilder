a371b82547f6ac3c4ac1f1e295358465fcf51196
Nataly Mironova <knightalli@mail.ru>
2025-01-10 00:18:00 +0500

    Add some info in HandManager (not working version)

--- Assets/Game/Hand/HandManager.cs
+++ Assets/Game/Hand/HandManager.cs
@@ -7,15 +7,13 @@ using Zenject;
 
 public class HandManager : MonoBehaviour
 {
-    public GameObject[] cardPrefabs;
-    public Transform handPanel;
-    public int minCards = 5;
-    public int maxCards = 8;
-    public float cardWidth = 183f;
-    public float handWidth = 1003f;
-
-    private List<GameObject> cardsInHand = new List<GameObject>();
+    [SerializeField] private Transform handPanel;
+    [SerializeField] private int minCards = 5;
+    [SerializeField] private int maxCards = 15;
+    
+    private List<PlayerCard> cardsInHand;
     private List<CardInDesk> cardInDeck;
+    private List<int> numberOfCardsInHand;
 
     private IInstantiator _instatiator;
 
@@ -30,32 +28,28 @@ public class HandManager : MonoBehaviour
         cardInDeck = FindObjectsOfType<CardInDesk>(true).ToList();
     }
 
-    public void DrawCardsInHand()
+    public void RedrawCardsInHand()
     {
-        ClearHand();
+        ClearHand();        
 
-        HorizontalLayoutGroup horizontalPanel = handPanel.GetComponent<HorizontalLayoutGroup>();
-
-        int cardCount = Random.Range(minCards, maxCards + 1);
-
-        if (cardCount == 5) horizontalPanel.spacing = 20f;
-        if (cardCount == 6) horizontalPanel.spacing = -20f;
-        if (cardCount == 7) horizontalPanel.spacing = -46f;
-        if (cardCount == 8) horizontalPanel.spacing = -66f;
-
+        int cardCount = Random.Range(minCards, maxCards + 1);   
 
         for (int i = 0; i < cardCount; i++)
         {
             int numberOfCard = Random.Range(0, cardInDeck.Count);
             string nameOfCard = cardInDeck[numberOfCard].PlayerCardData.name;
             Sprite imageOfCard = cardInDeck[numberOfCard].PlayerCardData.Sprite;
-            print(nameOfCard);
 
             if (cardInDeck[numberOfCard].CountOfCard >= 0)
             {
                 AddCard(nameOfCard, imageOfCard);
+                numberOfCardsInHand.Add(numberOfCard);
                 cardInDeck[numberOfCard].CountOfCard--;
-            }            
+            }
+            else
+            {
+                cardCount++;
+            }
         }
     }
 
@@ -70,20 +64,48 @@ public class HandManager : MonoBehaviour
         Image cardImage = newCard.GetComponent<Image>();
         cardImage.sprite = imageOfCard;
 
-        cardsInHand.Add(newCard.gameObject);
+        cardsInHand.Add(newCard);        
     }
 
-    public void DeleteCard(GameObject card)
+    public void ReturnCardsInDeck()
     {
-        
+        foreach (int number in numberOfCardsInHand)
+        {
+            cardInDeck[number].CountOfCard++;
+        }
+        ClearHand();
+    }
+
+    public void DeleteCard(PlayerCard card)
+    {
+        Destroy(card.gameObject);
     }
 
     public void ClearHand()
     {
-        foreach (GameObject card in cardsInHand)
+        foreach (PlayerCard card in cardsInHand)
         {
-            Destroy(card);
+            DeleteCard(card);
         }
         cardsInHand.Clear();
+        numberOfCardsInHand.Clear();
+    }
+
+    public void UpdateSpacing()
+    {
+        int cardCount = cardsInHand.Count;
+        HorizontalLayoutGroup horizontalPanel = handPanel.GetComponent<HorizontalLayoutGroup>();
+
+        if (cardCount == 5) horizontalPanel.spacing = 20f;
+        if (cardCount == 6) horizontalPanel.spacing = -20f;
+        if (cardCount == 7) horizontalPanel.spacing = -46f;
+        if (cardCount == 8) horizontalPanel.spacing = -66f;
+        if (cardCount == 9) horizontalPanel.spacing = -78f;
+        if (cardCount == 10) horizontalPanel.spacing = -89f;
+        if (cardCount == 11) horizontalPanel.spacing = -99f;
+        if (cardCount == 12) horizontalPanel.spacing = -106f;
+        if (cardCount == 13) horizontalPanel.spacing = -112f;
+        if (cardCount == 14) horizontalPanel.spacing = -117f;
+        if (cardCount == 15) horizontalPanel.spacing = -122f;
     }
 }
